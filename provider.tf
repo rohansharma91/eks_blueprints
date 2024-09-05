a188581b8d0b173e5f872244d39f55fdbcd5119d
rohansharma91 <115484779+rohansharma91@users.noreply.github.com>
2024-09-05 14:09:39 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -32,17 +32,6 @@ provider "kubernetes" {
   token                  = data.aws_eks_cluster_auth.this.token
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = module.eks_blueprints.eks_cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
-
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", module.eks_blueprints.eks_cluster_id]
-  }
-}
 }
 provider "kubectl" {
   host                   = module.eks_blueprints.eks_cluster_endpoint
