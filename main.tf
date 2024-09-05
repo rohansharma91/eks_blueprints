d1dfa555f0ef62cf797e00be8710bb795ba706ad
rohansharma91 <115484779+rohansharma91@users.noreply.github.com>
2024-09-05 15:01:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -68,38 +68,21 @@ resource "time_sleep" "wait_for_cluster" {
   }
 }
 
-module "eks_blueprints_addons" {
-  source = "aws-ia/eks-blueprints-addons/aws"
-  version = "1.16.3"
+module "kubernetes_addons" {
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons?ref=v4.0.2"
 
-  
   eks_cluster_id = module.eks_blueprints.eks_cluster_id
 
-  eks_addons = {
-    aws-ebs-csi-driver = {
-      most_recent = false
-    }
-    coredns = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-  }
+  # EKS Add-ons
+  enable_amazon_eks_vpc_cni            = true
+  enable_amazon_eks_coredns            = true
+  enable_amazon_eks_kube_proxy         = true
+  enable_amazon_eks_aws_ebs_csi_driver = true
 
-  enable_aws_load_balancer_controller    = true
-  enable_cluster_proportional_autoscaler = false
-  enable_karpenter                       = false
-  enable_kube_prometheus_stack           = false
-  enable_metrics_server                  = false
-  enable_external_dns                    = true
-  enable_cert_manager                    = false
-  
-
-  tags = {
-    Environment = "dev"
-  }
+  # Self-managed Add-ons
+  enable_aws_for_fluentbit            = false
+  enable_aws_load_balancer_controller = true
+  enable_aws_efs_csi_driver           = false
+  enable_cluster_autoscaler           = false
+  enable_metrics_server               = false
 }
