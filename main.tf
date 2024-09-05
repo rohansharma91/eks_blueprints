802bdbf27db7303c7c3536232a21edde3b3f6ba2
rohansharma91 <115484779+rohansharma91@users.noreply.github.com>
2024-09-05 13:10:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -94,15 +94,4 @@ module "kubernetes_addons" {
   }
 }
 
-provider "helm" {
-  kubernetes {
-    host                   = module.eks_blueprints.eks_cluster_endpoint
-    cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
 
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", module.eks_blueprints.eks_cluster_id]
-    }
-  }
-}
