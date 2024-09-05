83d295f68fd0b7bd8731d932a1f7d818a945c80b
rohansharma91 <115484779+rohansharma91@users.noreply.github.com>
2024-09-05 14:10:26 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -30,8 +30,7 @@ provider "kubernetes" {
   host                   = module.eks_blueprints.eks_cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks_blueprints.eks_cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.this.token
-}
-
+ }
 }
 provider "kubectl" {
   host                   = module.eks_blueprints.eks_cluster_endpoint
