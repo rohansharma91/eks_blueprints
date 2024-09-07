b41edd8d52ddc83d72ab790b2f4b70f177e37f15
rohansharma91 <115484779+rohansharma91@users.noreply.github.com>
2024-09-07 10:40:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -69,66 +69,21 @@ resource "time_sleep" "wait_for_cluster" {
 }
 
 module "eks_blueprints_kubernetes_addons" {
-  source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons?ref=v4.8.0"
+  source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons?ref=v4.0.2"
 
   eks_cluster_id = module.eks_blueprints.eks_cluster_id
 
-  #region EKS ADDONS
-  enable_amazon_eks_vpc_cni = true
-  enable_amazon_eks_coredns = true
-  amazon_eks_coredns_config = {
-    most_recent        = true
-    kubernetes_version = "1.30"
-    resolve_conflicts  = "OVERWRITE"
-  }
+  # EKS Addons
+  enable_amazon_eks_vpc_cni            = true
+  enable_amazon_eks_coredns            = true
   enable_amazon_eks_kube_proxy         = true
   enable_amazon_eks_aws_ebs_csi_driver = true
-  #endregion
 
-  #region K8s ADDONS
-  enable_argocd = false
-   
-  argocd_helm_config = {
-    set_sensitive = [
-      {
-        name  = "configs.secret.argocdServerAdminPassword"
-        value = bcrypt(data.aws_secretsmanager_secret_version.admin_password_version.secret_string)
-      }
-    ]
-  }
-
-  argocd_manage_add_ons = true # Indicates that ArgoCD is responsible for managing/deploying add-ons
-  argocd_applications = {
-    addons = {
-      path               = "chart"
-      repo_url           = "https://github.com/aws-samples/eks-blueprints-add-ons.git"
-      add_on_application = true
-    }
-    workloads-dev = {
-      path               = "argocd-apps/dev"
-      repo_url           = "hhttps://github.com/rohansharma91/eks_blueprints_workloads.git"
-      add_on_application = false
-    }
-  }
-
-  
-
-  # ingress
+  #K8s Add-ons
+  enable_argocd                       = true
+  enable_aws_for_fluentbit            = true
   enable_aws_load_balancer_controller = true
-  
-
-  enable_aws_for_fluentbit            = false
-  enable_cluster_autoscaler           = false
-  enable_metrics_server               = false
-  enable_prometheus                   = false
-  enable_grafana                      = false
-  enable_external_secrets             = false
-  enable_aws_efs_csi_driver           = false
-  enable_aws_cloudwatch_metrics       = false
-  #endregion
-
-  depends_on = [
-    time_sleep.wait_for_cluster
-  ]
+  enable_cluster_autoscaler           = true
+  enable_metrics_server               = true
+  enable_prometheus                   = true
 }
-#endregion
